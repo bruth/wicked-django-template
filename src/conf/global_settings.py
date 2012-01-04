11e3379d9477c8ef939c48b64387c8001513592d
Byron Ruth <b@devel.io>
2012-01-04 13:48:08 -0500

    Uncomment AuthenticationMiddleware Required by the admin app

--- src/conf/global_settings.py
+++ src/conf/global_settings.py
@@ -182,7 +182,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'siteauth.middleware.SiteAuthenticationMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
