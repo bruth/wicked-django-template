8c998e10666cfb5a6d3a88ceef4390adb1d632cc
Byron Ruth <b@devel.io>
2012-09-20 21:00:30 -0400

    Remove arbitrary scripts from build script

--- src/static/scripts/javascript/app.build.js
+++ src/static/scripts/javascript/app.build.js
@@ -2,32 +2,22 @@
 // Full example: https://github.com/jrburke/r.js/blob/master/build/example.build.js
 
 ({
-    // optiize relative to this url (i.e. the current directory)
+    // Optimize relative to this url (i.e. the current directory)
     baseUrl: '.',
     
-    // the source directory of the modules
+    // The source directory of the modules
     appDir: 'src',
 
-    // the target directory of the optimized modules
+    // The target directory of the optimized modules
     dir: 'min',
 
     optimize: 'uglify',
 
     optimizeCss: 'none',
 
-    // a reference here for any scripts that need to include cilantro
+    // A reference here for any scripts that need to include cilantro
     // modules
-    paths: {
-        'cilantro/main': 'empty:'
-    },
+    paths: {},
     
-    // if the project requires any custom javascript modules, they can be
-    // listed here. below shows an example for a 'main' module. if this is
-    // defined and depends on the 'cilantro/main' module, ensure this is
-    // excluded during the optimization step.
-    
-    // modules: [{
-    //     name: 'main',
-    //     exclude: ['cilantro/main']
-    // }]
+    modules: [{}]
 })
