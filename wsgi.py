55b4284f82de2b763c79bc2403d8c1d7a3b863da
Byron Ruth <b@devel.io>
2012-10-04 09:10:09 -0400

    Ensure the project root is on sys.path

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.conf.settings')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/tmp')
