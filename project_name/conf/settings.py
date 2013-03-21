3544f2791f7d5ceb3359ca88ef4d937fce766f5f
Byron Ruth <b@devel.io>
2013-03-21 19:33:50 -0400

    Ran 2to3

--- project_name/conf/settings.py
+++ project_name/conf/settings.py
@@ -1,8 +1,8 @@
 import os
-from global_settings import *
+from .global_settings import *
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     import warnings
     warnings.warn('Local settings have not been found ({{ project_name }}.conf.local_settings)')
